7da4c9efa1039648f6dd722a629e46b8818b9785
Anna Czarnocka <a.czarnocka8@student.uw.edu.pl>
2024-04-02 19:14:55 -0700

    Updated modelling.py file, deleting comments

--- script/modelling.py
+++ script/modelling.py
@@ -13,7 +13,6 @@ from sklearn.linear_model import LogisticRegression
 @click.argument('input_file')
 @click.argument('figure_prefix')
 def modelling(input_file, figure_prefix):
-    # Ensure the Visualizations directory exists
     results_dir = 'results'
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
@@ -21,12 +20,9 @@ def modelling(input_file, figure_prefix):
     # Read the data
     df = pd.read_csv(input_file)
 
-    # Prepare the data
-    # Assuming 'Price' is your target variable based on your description
     target = 'Price'
     X_train, X_test, y_train, y_test = split_data(df, target, test_size=0.2, random_state=42)
 
-    # Train model - example with LogisticRegression, adjust according to your needs
     model = LogisticRegression()
     trained_model = train_model(model, X_train, y_train)
 
